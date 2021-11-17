cbfd8897527cf004868ae912460beb8d0b407d96
LiarCoder <liarwhen@qq.com>
2021-11-18 01:30:50 +0800

    docs main: 增加了对堆排序中的函数的说明

--- sorting-algorithm/select/heap-sort.js
+++ sorting-algorithm/select/heap-sort.js
@@ -4,11 +4,21 @@
  * @Author: LiarCoder
  * @Date: 2021-11-17 22:16:43
  * @LastEditors: LiarCoder
- * @LastEditTime: 2021-11-18 00:45:02
+ * @LastEditTime: 2021-11-18 01:28:22
  */
 
+/**
+ * @description: 堆排序 
+ * @param {Array} arr 待排序数组
+ * @return {*} undefined
+ */
 
 function heapSort(arr) {
+  /**
+   * 因为堆排序中需要利用数组的下标关系构建一棵完全二叉树，而如果起始下标为0的话就会导致后续
+   * 计算出错，所以第一个有效元素的下标应该从1开始，所以需要在进行堆排序前特意增加一个头部元
+   * 素来占位，当然，这个多余的头部元素也可以替代下面temp变量的作用，这样就不会浪费空间了
+   */
   arr.unshift(0);
   initHeap(arr);
   for (let i = arr.length - 1; i >= 2; i--) {
@@ -18,7 +28,22 @@ function heapSort(arr) {
   arr.shift();
 }
 
+/**
+ * @description: updateHeap 是用于重建一个堆结构（数组形式）的函数
+ * @param {heap: array} 需要重建的堆（该堆是用数组结构存储的）
+ * @param {root: number} 需要重建的堆的根节点在heap数组中的下标
+ * @param {rear: number} 需要重建的堆的最后一个节点在heap数组中的下标
+ * @return {*}
+ */
+
 function updateHeap(heap, root, rear) {
+  /**
+   * temp：用于暂存需要重建的堆的根元素，可以用heap[0]代替，反正heap[0]闲着也是闲着
+   * finished：用于标识是否找到了根元素的归宿
+   * parent：用于标识一个堆结构中的某个父节点
+   * child：用于标识上面的parent的子节点，初始值是左子节点的下标，后续将会根据parent的两个
+   *        子节点的值的大小做更新，反正要保证它标识的是较大的那个子节点
+   */
   let temp = heap[root];
   let finished = false;
   let parent = root;
@@ -35,6 +60,12 @@ function updateHeap(heap, root, rear) {
   }
   heap[parent] = temp;
 }
+
+/**
+ * @description: initHeap用于初始化一个堆
+ * @param {heap: array} 需要初始化的堆（该堆是用数组结构存储的）
+ * @return {*}
+ */
 
 function initHeap(heap) {
   for (let i = Math.floor((heap.length - 1) / 2); i >= 1; i--) {
